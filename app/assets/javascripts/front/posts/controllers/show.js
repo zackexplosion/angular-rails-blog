7d8a16e9b015b1f1dd31461987b571dedfedd126
Tony's Macbook <cstony0917@gmail.com>
2015-05-20 16:52:03 +0800

    code hightlight for post content

--- app/assets/javascripts/front/posts/controllers/show.js
+++ app/assets/javascripts/front/posts/controllers/show.js
@@ -1,7 +1,74 @@
 angular
 .module('blog')
 .controller('PostShowCrtl', 
-[        '$scope', 'POST', 
-function( $scope,   POST){
-   $scope.post = POST;
+[        '$rootScope', '$timeout', '$scope', 'POST', 
+function( $rootScope,   $timeout,   $scope,   POST){
+    $scope.post = POST;
+
+    // var a = $scope.post.$promise.then(function(){
+    //   debugger;  
+    // });
+    
+
+    var hightlight_codes = function(){
+      var codes = $('pre code');
+
+      if(codes.length == 0){
+        $timeout(function(){
+          // debugger;
+          hightlight_codes();
+        });
+        return;
+      }
+      
+      codes.each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
+    }
+
+    hightlight_codes();
+
+    // $scope.$watch('post.contet',function(){
+    //   debugger;
+    // }, true)
+
+    // $timeout(function(){
+    //   // debugger;
+    //   hightlight_codes();
+    // });
+
+
+    // angular.element(document).ready(function () {
+    //   $('pre code').each(function(i, block) {
+    //     console.log(block);
+    //     hljs.highlightBlock(block);
+    //   });
+    // });
+
+    // $rootScope.$on('$viewContentLoaded', function (event) {
+    //   // console.log('lock & loaded')
+
+    //   hightlight_codes();
+    // });
+
+
+
+    // var render_highlight = function(){
+    //   codes.each(function(i, block) {
+    //     console.log(block);
+    //     hljs.highlightBlock(block);
+    //   });
+    // }
+
+    // // var codes = $('pre code');
+    // if(codes.length === 0){
+    //   setTimeout(function(){
+    //     render_highlight();
+    //   });
+    // }else{
+    //   render_highlight()
+    // }
+
+
+
 }])
