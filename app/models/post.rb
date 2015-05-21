8cc8666a7975c4e2a8bc735a17b8278375ff47ae
Tony's Macbook <cstony0917@gmail.com>
2015-05-21 15:54:33 +0800

    improve content substr function to preview

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,28 @@
 class Post < ActiveRecord::Base
 
-
   def html_content
     self.markdown self.content
   end
 
   def short_html_content
-    self.markdown self.content[0..50]
+    content = ''
+    c = self.content.split("\n")
+    max_line = 10
+
+    if max_line < c.length
+      for i in 0..max_line
+        content += c[i].to_s + "\n"
+      end
+      content += '...'
+    else
+      content = self.content
+    end
+
+
+
+    # asd
+
+    self.markdown content
   end
 
   def markdown content
