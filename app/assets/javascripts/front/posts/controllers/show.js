29e464ef6c54aabdbd1c73f046c71fbc7adf8506
Tony's Macbook <cstony0917@gmail.com>
2015-05-21 14:52:23 +0800

    install disqus

--- app/assets/javascripts/front/posts/controllers/show.js
+++ app/assets/javascripts/front/posts/controllers/show.js
@@ -9,4 +9,23 @@ function( $rootScope,   HightlightCodes,   $scope,   POST){
 
     HightlightCodes();
 
+
+
+
+    /*
+    #################################
+    ##########    disqus   ##########
+    #################################
+    */
+    
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'zackexplosion';
+    
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+
 }])
