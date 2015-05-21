1a5ef3c8e8ccd0a17c41b23cc2d5d148c579ac8f
Tony's Macbook <cstony0917@gmail.com>
2015-05-21 12:45:28 +0800

    improve show.js

--- app/assets/javascripts/front/posts/controllers/show.js
+++ app/assets/javascripts/front/posts/controllers/show.js
@@ -1,25 +1,12 @@
 angular
 .module('blog')
 .controller('PostShowCrtl', 
-[        '$rootScope', '$timeout', '$scope', 'POST', 
-function( $rootScope,   $timeout,   $scope,   POST){
+[        '$rootScope', 'HightlightCodes', '$scope', 'POST', 
+function( $rootScope,   HightlightCodes,   $scope,   POST){
     $scope.post = POST;
 
     $rootScope.title = POST.title + ' | ';
 
-    var hightlight_codes = function(){
-      var codes = document.querySelectorAll('pre code');      
-      if(codes.length === 0){
-        $timeout(function(){
-          // debugger;
-          hightlight_codes();
-        });
-        return;
-      }
-      angular.forEach(codes,function(block){
-        hljs.highlightBlock(block);
-      })
-    }
-    hightlight_codes();
+    HightlightCodes();
 
 }])
