b67ae8ddf9100a17c18ef6e7caf3354fc21c02a6
Tony's Macbook <cstony0917@gmail.com>
2015-05-22 16:01:42 +0800

    use github to host assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_controller.asset_host = 'http://cstony0917.github.io/zackexplosion/'
 end
