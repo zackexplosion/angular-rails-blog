0b32e99af150611f689c86669fc19065d4104a76
Tony's Macbook <cstony0917@gmail.com>
2015-05-22 16:17:55 +0800

    update assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.asset_host = 'http://cstony0917.github.io/zackexplosion/'
+  config.action_controller.asset_host = '//cstony0917.github.io/zackexplosion/'
 end
