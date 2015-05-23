e839f8468fb041eed9b2fe9f6016ddddd06cd764
Tony's Hackintosh <cstony0917@gmail.com>
2015-05-23 18:35:00 +0800

    show live only on medium screen devices

--- app/assets/javascripts/front/application.js
+++ app/assets/javascripts/front/application.js
@@ -29,6 +29,20 @@ angular
 [        '$rootScope', 
 function( $rootScope){
     $rootScope.title = '';
+    $rootScope.window = {
+        width  : Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        height : Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+    };
+
+    if( $rootScope.window.width >= 768){
+        var iframe = '<iframe src="https://livehouse.in/embed/channel/117413" frameborder="0" allowfullscreen></iframe>';
+        angular
+        .element(document.querySelector('#livehouse .video-wrapper'))
+        .html(iframe);
+    }
+    
+    
+    // debugger;
 }])
 .constant('Path', {
     'template': '/templates?t=front'
