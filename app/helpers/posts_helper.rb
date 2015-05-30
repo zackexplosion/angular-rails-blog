387ba8e655bb98d22f476f18d5ac7d0bd40177bd
Tony's Hackintosh <cstony0917@gmail.com>
2015-05-30 13:25:52 +0800

    improve index json content

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def images p
+    html_content = Nokogiri::HTML(p.rendered_content)
+
+    images = []
+
+    html_content.css('img').each do |i|
+      images.push i['src']
+    end
+    return images
+  end
 end
--- app/views/posts/index.jbuilder
+++ app/views/posts/index.jbuilder
@@ -1,4 +1,6 @@
 json.array! @posts do |p|
-  json.merge! p.attributes
+  json.merge! p.attributes.except('rendered_content')
   json.content p.short_html_content
+ 
+  json.images  images(p)  
 end
