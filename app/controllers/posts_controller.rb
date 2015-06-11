5ab7d486dd81495724d2b187a10951abbf9165ff
Tony's Macbook <cstony0917@gmail.com>
2015-06-11 11:23:41 +0800

    add allow origin to all to get blog posts list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   def index
+    response.headers["Access-Control-Allow-Origin"] = "*"
     # @posts = Post.where(:state => 'published').order('id desc')
-    @posts = Post.list params[:page]    
+    @posts = Post.list params[:page]
   end
 
   def show
