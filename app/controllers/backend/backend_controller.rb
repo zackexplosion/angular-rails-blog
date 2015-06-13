721bd453571820f6ac86ebb8e49837becef0f95b
Tony's Hackintosh <cstony0917@gmail.com>
2015-06-14 02:19:55 +0800

    disable cache on backend controller

--- app/controllers/backend/backend_controller.rb
+++ app/controllers/backend/backend_controller.rb
@@ -1,5 +1,10 @@
 class Backend::BackendController < ApplicationController
   before_action :authenticate_user!
+  before_action :disable_cache
+
+  def disable_cache
+    expires_in 0.minutes
+  end
   
   def index
     render :template => 'layouts/backend.html.erb', :layout => false
